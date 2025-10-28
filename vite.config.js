7b158a6892eac5ea4095a33c67d8b3177e46d1eb
Kiretychowdary <kiretyv@gmail.com>
2025-10-29 00:31:43 +0530

    Added the new things

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 // /NMKRSVPLIDATAPERMANENT
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-router-dom']
+  }
 })
