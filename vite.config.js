f1ccc6c3cff18f111d4e02e0b07e57f656f87ee1
Kiretychowdary <kiretyv@gmail.com>
2025-10-29 00:17:14 +0530

    resolved the deployment error in vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 // /NMKRSVPLIDATAPERMANENT
+// vite.config.js
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-router', 'react-router-dom'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-router/, /node_modules/],
+    },
+  },
 })
